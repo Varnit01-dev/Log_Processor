72ebb874288abd62eccf2f3f0b9d47433d61fc44
Varnit Sharma <94976653+Varnit01-dev@users.noreply.github.com>
2024-07-20 22:31:38 +0530

    Update log_collector.go

--- log_collector.go
+++ log_collector.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"log"
-	"net/http" 
-	"fmt"
+	"net/http"
 )
 
 type LogEntry struct {
-	Message string `json:"message"`
-	Level   string `json:"level"`
-}     
-
-
- 
+	Message string `json:"message" xml:"message"`
+	Level   string `json:"level" xml:"level"`
+}
 
 func main() {
 	http.HandleFunc("/logs", logsHandler)
@@ -22,11 +19,28 @@ func main() {
 
 func logsHandler(w http.ResponseWriter, r *http.Request) {
 	var logEntry LogEntry
-	err := json.NewDecoder(r.Body).Decode(&logEntry)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+
+	contentType := r.Header.Get("Content-Type")
+	switch contentType {
+	case "application/json":
+		err := json.NewDecoder(r.Body).Decode(&logEntry)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	case "application/xml":
+		err := xml.NewDecoder(r.Body).Decode(&logEntry)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	default:
+		http.Error(w, "Unsupported Content-Type", http.StatusUnsupportedMediaType)
 		return
 	}
+
 	log.Printf("%s: %s", logEntry.Level, logEntry.Message)
 	w.WriteHeader(http.StatusAccepted)
 }
+
+
