2eb1432355a4167cec39a206d49c7d3b7cd582dc
Varnit Sharma <94976653+Varnit01-dev@users.noreply.github.com>
2024-07-24 12:51:39 +0530

    Add files via upload

---
+++ logparser.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func ParseLog() {
+	logsExample := `[2024-07-27T07:39:54.173Z] "GET /healthz HTTP/1.1" 200 - 0 61 225 - "111.114.195.106,10.0.0.11" "okhttp/3.12.1" "0557b0bd-4c1c-4c7a-ab7f-2120d67bee2f" "example.com" "172.16.0.1:8080"`
+
+	// Define log format
+	logFormat := `\[(?P<time_stamp>[^]]+)\] "(?P<http_method>\w+) (?P<request_path>[^ ]+) HTTP\/\d\.\d" (?P<response_code>\d{3}) - (?P<bytes_sent>\d+) (?P<duration>\d+) (?P<request_length>\d+) - "(?P<ips>[^"]+)" "(?P<user_agent>[^"]+)" "(?P<request_id>[^"]+)" "(?P<domain>[^"]+)" "(?P<client_ip>[^"]+:\d+)"`
+
+	// Compile the regex pattern
+	re := regexp.MustCompile(logFormat)
+
+	// Find matches in the log example
+	matches := re.FindStringSubmatch(logsExample)
+
+	// Create a map to store named captures
+	captureGroups := make(map[string]string)
+	names := re.SubexpNames()
+
+	// Populate the map with captured groups
+	for i, name := range names {
+		if i != 0 && name != "" {
+			captureGroups[name] = matches[i]
+		}
+	}
+
+	// Print the captured groups
+	for key, value := range captureGroups {
+		fmt.Printf("%-15s => %s\n", key, value)
+	}
+}
+
+func main() {
+	ParseLog()
+}
